27a2b7720fb5466f5f200903b3192d95deb31f95
Ceezik <ceezik.pro@gmail.com>
2021-03-14 11:39:04 +0100

    setup eslint in background

--- background/src/App.tsx
+++ background/src/App.tsx
@@ -1,5 +1,5 @@
-import React from "react";
+import React from 'react';
 
 export default function App(): JSX.Element {
-  return <h1>Uber eats stats background</h1>;
+    return <h1>Uber eats stats background</h1>;
 }

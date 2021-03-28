c63993b1bdff837df0497f6fb01ad13672793ed7
Ceezik <ceezik.pro@gmail.com>
2021-03-28 19:24:22 +0200

    setup eslint in popup

--- popup/src/index.tsx
+++ popup/src/index.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import App from "./app";
-import configureAppStore from "./store/configureStore";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './app';
+import configureAppStore from './store/configureStore';
 
 const store = configureAppStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
